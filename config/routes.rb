2a652ee6e209d049425be4af47be28e26be64847
綾乃 <>
2022-04-24 23:25:03 +0900

    カート内の商品を注文できるようにする

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
     collection do
       get "cart", :to => "shopping_carts#index"
       post "cart/create", :to => "shopping_carts#create"
+      delete "cart", :to => "shopping_carts#destroy"
       get "mypage/show", :to => "users#show"
       get "mypage", :to => "users#mypage"
       get "mypage/edit_password", :to =>"users#edit_password"
