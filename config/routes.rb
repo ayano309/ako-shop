6398270c8e870aa56b4f8dc2fc115c5795fda6c8
綾乃 <>
2022-04-25 18:36:00 +0900

    [create]管理者モデル,ログイン,ログアウトのルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
   
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "products#index"
+  # 管理者関連
+  devise_for :admins, :controllers => {
+    :sessions => 'admins/sessions'
+  }
 
+  devise_scope :admin do
+    get "dashboard", :to => "dashboard#index"
+    get "dashboard/login", :to => "admins/sessions#new"
+    post "dashboard/login", :to => "admins/sessions#create"
+    delete "dashboard/logout", :to => "admins/sessions#destroy"
+  end
+
+
+  # 商品関連
+  root to: "products#index"
   resources :products do
     resources :reviews, only: [:create]
     member do
@@ -10,8 +22,8 @@ Rails.application.routes.draw do
     end
   end
 
-  get "dashboard", :to => "dashboard#index"
-
+  
+  # user関連
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions',
@@ -20,11 +32,15 @@ Rails.application.routes.draw do
     :unlocks => 'users/unlocks',
   }
 
+  # userのサインアップなど
+
   devise_scope :user do
     get "signup", :to => "users/registrations#new"
     get "login", :to => "users/sessions#new"
     delete "logout", :to => "users/sessions#destroy"
   end
+
+  # userに関するページ
 
   resource :users, only: [:show] do
     collection do
