b46e384da6ca5dec32c9e1ab57727c2dce243de9
綾乃 <>
2022-06-17 22:56:03 +0900

    [add]管理者ゲストログイン

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,11 @@
 class Admin < ApplicationRecord
   devise :database_authenticatable, :rememberable, :validatable
   # deviseのログイン機能とバリデーション機能だけが有効
+
+  def self.guest
+    find_or_create_by!(name: 'guestadmin' ,email: 'admin@example.com') do |admin|
+      admin.password = SecureRandom.urlsafe_base64
+      admin.name = "guestadmin"
+    end
+  end
 end
